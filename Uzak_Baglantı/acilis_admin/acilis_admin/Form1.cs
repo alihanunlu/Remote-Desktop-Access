165eac1b82c5aec361aad7a0aadab7ae24c7fe46
Alihan ÜNLÜ <alihan3134@gmail.com>
2021-03-09 19:31:37 +0300

    Uzak Baglanti

---
+++ Uzak_Baglantı/acilis_admin/acilis_admin/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace acilis_admin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection adminbgln = new SqlConnection("Data Source = alihan.database.windows.net; Initial Catalog = alihahnunlu; User ID = unlu; Password=Hatay.31");
+            // TODO: Bu kod satırı 'alihahnunluDataSet.vt_admin' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.vt_adminTableAdapter.Fill(this.alihahnunluDataSet.vt_admin);
+            adminbgln.Open();
+            SqlCommand ekleveri = new SqlCommand("select * from vt_admin",adminbgln);
+            SqlDataReader dr1 = ekleveri.ExecuteReader();
+            DataTable tb1 = new DataTable();
+            tb1.Load(dr1);
+            dataGridView1.DataSource = tb1;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection adminbgln = new SqlConnection("Data Source = alihan.database.windows.net; Initial Catalog = alihahnunlu; User ID = unlu; Password=Hatay.31");
+            adminbgln.Open();
+            SqlCommand silveri = new SqlCommand("delete from vt_admin where id='"+dataGridView1.CurrentRow.Cells[0].Value.ToString()+"'", adminbgln);
+            silveri.ExecuteNonQuery();
+            Form1_Load(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("PC Kapatıldı");
+        }
+    }
+}
---
+++ Uzak_Baglantı/time_vt/time_vt/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Win32;
+
+namespace time_vt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection vtbgln = new SqlConnection("Data Source = alihan.database.windows.net; Initial Catalog = alihahnunlu; User ID = unlu; Password=Hatay.31");
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'alihahnunluDataSet1.acilis_saat' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.acilis_saatTableAdapter.Fill(this.alihahnunluDataSet1.acilis_saat);
+            timer1.Start();
+            this.acilis_saatTableAdapter.Fill(this.alihahnunluDataSet1.acilis_saat);
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            vtbgln.Open();
+            SqlCommand eklesrgu = new SqlCommand("INSERT INTO acilis_saat(time) values('" + DateTime.Now.ToLongTimeString() + "')", vtbgln);
+            eklesrgu.ExecuteNonQuery();
+            vtbgln.Close();
+            RegistryKey baslangıc = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            baslangıc.SetValue("Saat", "\"" + Application.ExecutablePath + "\"");
+        }
+    }
+}
